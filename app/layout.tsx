4f3f4d9b76ac7b77faae7c785b7e7a6647e3ad6f
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-12 15:27:31 +0600

    add the theme provider on layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeSwitcher } from "@/components/ui/theme-switcher";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <ThemeSwitcher/>
+         <ThemeProvider attribute="class" disableTransitionOnChange>
+          {children}
+          <ThemeSwitcher />
+        </ThemeProvider>
       </body>
     </html>
   );
