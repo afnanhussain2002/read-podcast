ef0e8f289248303c02e341378267b8889a0b14a5
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-15 15:07:27 +0600

    start write the authorized func

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,14 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
-            
+           name: "Credentials",
+           credentials: {
+               email: { label: "Email", type: "email" },
+               password: { label: "Password", type: "password" },
+           },
+          async authorize(credentials) {
+               
+           },
         })
     ],
 }
