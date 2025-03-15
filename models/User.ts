7bafeda8bdc3ee08af88d12cb5ede46c9e6b03d5
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-15 14:34:54 +0600

    write the data type of user

---
+++ models/User.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+
+export interface IUser {
+    email: string;
+    password: string;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
