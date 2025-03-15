4abba0fe56f80e2db72ad7b3f29920b2df2e024e
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-15 15:12:55 +0600

    find the user with email

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,8 @@
+
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -15,7 +18,9 @@ export const authOptions: NextAuthOptions = {
                }
 
                try {
-                
+                await connectToDatabase();
+
+                const user = await User.findOne({ email: credentials.email });
                } catch (error) {
                 
                }
