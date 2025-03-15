605a98249fe66a51d9bfeab174297d8611a72ad8
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-15 15:05:07 +0600

    import and write credentials provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,10 @@
 import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
-
+    providers: [
+        CredentialsProvider({
+            
+        })
+    ],
 }
