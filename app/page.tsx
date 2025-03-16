a62b4bdf58a682ed7ec1f1b289f015a74c63e176
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-16 15:18:27 +0600

    successfully logged the user

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
 
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
   return (
     <header className="dark:bg-secondaryBlack inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
     <div className="mx-auto w-container max-w-full px-5 py-[110px] text-center lg:py-[150px]">
@@ -19,6 +26,11 @@ export default function Home() {
       its going to blow your mind!
       
       </p>
+      {session?.user ? (
+        <p>Logged in as: {session.user.email}</p>
+      ) : (
+        <p>Not logged in</p>
+      )}
     </div>
   </header>
   );
--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes'
+import { SessionProvider } from "next-auth/react"
 
 
 import * as React from 'react'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <SessionProvider><NextThemesProvider {...props}>{children}</NextThemesProvider></SessionProvider>
 }
