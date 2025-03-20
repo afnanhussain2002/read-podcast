3ec620f7e1b2cf0e2a9d597b9a0ab115cb75b100
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-20 23:25:35 +0600

    add a validation if video not available

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,12 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
     const {audio} = await request.json()
+
+    if (!audio) {
+        return NextResponse.json(
+            { error: "Audio is required" },
+            { status: 400 }
+        )
+    }
 }
