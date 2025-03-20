1228b81a4cd9250b974039f231658927284b950a
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-20 23:40:31 +0600

    npm i assemblyai

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,12 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-    const {audio} = await request.json()
 
-    if (!audio) {
+export async function POST(request: NextRequest) {
+    const {video} = await request.json()
+
+    if (!video) {
         return NextResponse.json(
             { error: "Audio is required" },
             { status: 400 }
         )
     }
+
+
 }
