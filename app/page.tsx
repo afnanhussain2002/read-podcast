2033190b301c7673d24944d528001023fa8fab80
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-04 11:32:22 +0600

    show the pricing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
 export default function Home() {
  
   return (
  <>
  <Hero/>
  <Features/>
+ <Pricing/>
  </>
   );
 }
