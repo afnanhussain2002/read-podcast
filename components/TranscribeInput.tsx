36ed2fe1486a8e456fcc9ee9db4d677280cd7eb7
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-05 07:05:06 +0600

    install select

--- components/TranscribeInput.tsx
+++ components/TranscribeInput.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Card, CardContent } from './ui/card'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
 
 const TranscribeInput = () => {
   return (
