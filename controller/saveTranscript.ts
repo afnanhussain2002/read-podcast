af62d5ebd73893cc363b73fb3273bcd81f4feed8
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-06 06:54:56 +0600

    connect to database

--- controller/saveTranscript.ts
+++ controller/saveTranscript.ts
@@ -1,8 +1,10 @@
 import { ISpeaker } from "@/dataTypes/transcribeDataTypes";
+import { connectToDatabase } from "@/lib/db";
+
 
 export default async function saveTranscript(transcript: string, confidence: number, speakers: ISpeaker[], OwnerId: string) {
      try {
-        
+        connectToDatabase();
      } catch (error) {
         
      }
