507b5cba7333a628982496b40760665f2e0cc842
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-06 10:45:48 +0600

    find the transcribe using id

--- controller/getSingleTranscript.ts
+++ controller/getSingleTranscript.ts
@@ -1,9 +1,12 @@
 import { connectToDatabase } from "@/lib/db";
+import Transcript from "@/models/Transcript";
 import { NextResponse } from "next/server";
 
 export default async function getSingleTranscript(transcriptId: string) {
     try {
         await connectToDatabase();
+
+        const transcript = await Transcript.findById(transcriptId);
     } catch (error) {
         NextResponse.json({ error: "Failed to get the transcript" }, { status: 500 });
     }
