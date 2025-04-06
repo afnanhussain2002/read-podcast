2100eb6c4faa82858e7a1f1235f35231ecebea0b
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-06 07:01:51 +0600

    write the full code for save data on database

--- controller/saveTranscript.ts
+++ controller/saveTranscript.ts
@@ -1,6 +1,7 @@
 import { ISpeaker } from "@/dataTypes/transcribeDataTypes";
 import { connectToDatabase } from "@/lib/db";
 import Transcript from "@/models/Transcript";
+import { NextResponse } from "next/server";
 
 
 export default async function saveTranscript(transcript: string, confidence: number, speakers: ISpeaker[], OwnerId: string) {
@@ -8,7 +9,13 @@ export default async function saveTranscript(transcript: string, confidence: num
         connectToDatabase();
 
         const transcribedData = await Transcript.create({transcript, confidence, speakers, OwnerId});
+
+        console.log(transcribedData, "Save on database");
+
+        return NextResponse.json({transcribedData,}, {status: 200});
+
      } catch (error) {
-        
+        console.log(error, "error from saving data on database");
+        NextResponse.json({error: "Failed to save data on database"}, {status: 500});
      }
 }
