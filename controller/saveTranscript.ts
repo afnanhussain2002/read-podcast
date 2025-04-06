1e6d129e60e076c1fc4b10f23b609a156463071c
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-06 06:56:51 +0600

    create transcript from database

--- controller/saveTranscript.ts
+++ controller/saveTranscript.ts
@@ -1,10 +1,13 @@
 import { ISpeaker } from "@/dataTypes/transcribeDataTypes";
 import { connectToDatabase } from "@/lib/db";
+import Transcript from "@/models/Transcript";
 
 
 export default async function saveTranscript(transcript: string, confidence: number, speakers: ISpeaker[], OwnerId: string) {
      try {
         connectToDatabase();
+
+        const transcribedData = await Transcript.create({transcript, confidence, speakers, OwnerId});
      } catch (error) {
         
      }
