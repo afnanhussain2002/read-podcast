3f3493bc3d658a1243ffe522c75d9852460f7bd7
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-06 10:44:55 +0600

    handle the error part

--- controller/getSingleTranscript.ts
+++ controller/getSingleTranscript.ts
@@ -1,9 +1,10 @@
 import { connectToDatabase } from "@/lib/db";
+import { NextResponse } from "next/server";
 
 export default async function getSingleTranscript(transcriptId: string) {
     try {
         await connectToDatabase();
     } catch (error) {
-        
+        NextResponse.json({ error: "Failed to get the transcript" }, { status: 500 });
     }
 }
