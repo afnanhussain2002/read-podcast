d36a23abafebed311b3f8e294b6d9569ce3dff82
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-06 06:54:27 +0600

    add a try catch block

--- controller/saveTranscript.ts
+++ controller/saveTranscript.ts
@@ -1,5 +1,9 @@
 import { ISpeaker } from "@/dataTypes/transcribeDataTypes";
 
 export default async function saveTranscript(transcript: string, confidence: number, speakers: ISpeaker[], OwnerId: string) {
-
+     try {
+        
+     } catch (error) {
+        
+     }
 }
