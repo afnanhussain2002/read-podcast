432c7f51b316c76b52e791dc39087e16739a7bb0
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-06 10:43:57 +0600

    connect with database

--- controller/getSingleTranscript.ts
+++ controller/getSingleTranscript.ts
@@ -1,6 +1,8 @@
+import { connectToDatabase } from "@/lib/db";
+
 export default async function getSingleTranscript(transcriptId: string) {
     try {
-        
+        await connectToDatabase();
     } catch (error) {
         
     }
