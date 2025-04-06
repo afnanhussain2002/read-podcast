1ed7ce112303c2a55e31c260e9eda9c787d9beae
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-06 06:53:27 +0600

    get the data on props for save on db

---
+++ controller/saveTranscript.ts
@@ -0,0 +1,5 @@
+import { ISpeaker } from "@/dataTypes/transcribeDataTypes";
+
+export default async function saveTranscript(transcript: string, confidence: number, speakers: ISpeaker[], OwnerId: string) {
+
+}
