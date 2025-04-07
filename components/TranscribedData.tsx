de995e7122511ac9cb0df5d0e8b769cdca3db170
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-07 10:48:06 +0600

    add use effect with transcribe data from local storage

--- components/TranscribedData.tsx
+++ components/TranscribedData.tsx
@@ -1,8 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TranscribedData = () => {
 
    const [transcript, setTranscript] = useState(null)
+
+   useEffect(() => {
+    const storedTranscript = localStorage.getItem('transcript');
+    if (storedTranscript) {
+      try {
+        const parsedTranscript = JSON.parse(storedTranscript);
+        setTranscript(parsedTranscript);
+      } catch (error) {
+        console.error('Failed to parse transcript from localStorage:', error);
+      }
+    }
+  }, []);
 
 
   return (
