3ed399771229a1168fff68eb81adda7cd54d6f41
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-08 21:37:35 +0600

    import needed files

---
+++ app/api/local-video-transcribe/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import multer from "multer";
+import ffmpegInstaller from "@ffmpeg-installer/ffmpeg";
+import ffmpeg from "fluent-ffmpeg";
+import fs from "fs";
+import path from "path";
+import { promisify } from "util";
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+// Configure Multer
+const upload = multer({
+    dest: "uploads/",
+})
+const uploadMiddleware = upload.single("video");
