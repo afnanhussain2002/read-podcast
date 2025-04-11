43c63325112a7c832dc3c9e0732de40279c8c410
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-11 07:26:45 +0600

    remove some text from google sign

--- components/GoogleSign.tsx
+++ components/GoogleSign.tsx
@@ -6,9 +6,6 @@ export default function GoogleSignIn() {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="bg-white rounded-lg shadow-lg w-full">
-        <h2 className="text-2xl font-bold text-center">Sign in</h2>
-        <p className="text-gray-600 text-center mb-3">Continue with Google</p>
-        
         <button
           onClick={() => signIn("google")}
           className="w-full flex items-center justify-center gap-2 bg-white text-gray-700 border border-gray-300 py-2 px-4 rounded-lg shadow-sm hover:bg-gray-100 transition-all"
