fac49129f252ab81ff5659f9a6492ef55239d29f
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-12 10:39:14 +0600

    remove some un used import code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,55 +6,15 @@ import { useSession, signOut } from "next-auth/react"
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
-
-const components = [
-  {
-    title: "Alert Dialog",
-    href: "https://ui.shadcn.com/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "https://ui.shadcn.com/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "https://ui.shadcn.com/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task.",
-  },
-  {
-    title: "Scroll-area",
-    href: "https://ui.shadcn.com/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "https://ui.shadcn.com/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "https://ui.shadcn.com/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element on hover.",
-  },
-]
 
 export default function Navbar() {
   const { data: session } = useSession()
