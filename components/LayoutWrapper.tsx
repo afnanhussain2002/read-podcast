a1911b697e2cdae6247a9e3d14dd4fe9bcfb8db0
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-12 15:02:10 +0600

    remove header and footer from login/register page

---
+++ components/LayoutWrapper.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const noLayoutRoutes = ['/login', '/register'];
+  const hideLayout = noLayoutRoutes.includes(pathname);
+
+  return (
+    <>
+      {!hideLayout && <Navbar />}
+      {children}
+      {!hideLayout && <Footer />}
+    </>
+  );
+}
