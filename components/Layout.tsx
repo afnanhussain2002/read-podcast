5b321cd06de3b94eaeaecfe37c63493aa9465e4f
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-12 12:29:41 +0600

    add navbar and footer

---
+++ components/Layout.tsx
@@ -0,0 +1,17 @@
+// components/Layout.tsx
+
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
