90f504e2d51edaec7557ddae400b6debfb6c879d
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-15 07:52:18 +0600

    get the logged user

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,6 +1,12 @@
+import { useSession } from 'next-auth/react';
 import React from 'react'
 
 const Dashboard = () => {
+
+      const { data: session } = useSession()
+      const user = session?.user
+    
+      console.log(user, "user");
   return (
     <div>Dashboard</div>
   )
