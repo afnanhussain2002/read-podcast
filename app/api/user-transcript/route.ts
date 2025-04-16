b834f88869ada99ac1add29c4039fb9b3f8d471f
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-16 07:18:51 +0600

    return un author if user not found

--- app/api/user-transcript/route.ts
+++ app/api/user-transcript/route.ts
@@ -1,6 +1,11 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
     const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
 }
