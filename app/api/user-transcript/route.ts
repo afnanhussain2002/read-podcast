110c783031e2722f1060c2c81e84cdde9306599c
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-16 07:18:10 +0600

    get thr session with auth options

--- app/api/user-transcript/route.ts
+++ app/api/user-transcript/route.ts
@@ -1,3 +1,6 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
 export async function GET(req: NextRequest) {
-    
+    const session = await getServerSession(authOptions);
 }
