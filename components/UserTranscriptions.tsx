0ca95935313fd7445c8f2a9ae7ebd25c4ebdda62
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-16 07:32:02 +0600

    fetch the all data

--- components/UserTranscriptions.tsx
+++ components/UserTranscriptions.tsx
@@ -1,9 +1,32 @@
 'use client';
+import { ITranscript } from '@/dataTypes/transcribeDataTypes';
 import React from 'react'
 
 import { useEffect, useState } from 'react';
 
 const UserTranscriptions = () => {
+    const [transcriptions, setTranscriptions] = useState<ITranscript[]>([]);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const fetchTranscriptions = async () => {
+        try {
+          const res = await fetch('/api/user-transcript');
+          const data = await res.json();
+          setTranscriptions(data.transcriptions || []);
+        } catch (error) {
+          console.error('Error fetching transcriptions:', error);
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchTranscriptions();
+    }, []);
+  
+    if (loading) return <p>Loading...</p>;
+  
+    if (transcriptions.length === 0) return <p>No transcriptions found.</p>;
   return (
     <div>UserTranscriptions</div>
   )
