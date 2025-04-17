598f6335578accd78db7158dc4a27a7c8554ea55
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-17 10:56:59 +0600

    add a new file for pricing plan

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,7 +1,6 @@
-import { Check } from 'lucide-react'
-import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
+
 import { pricingPlans } from '@/public/data'
+import PricingPlan from './PricingPlan'
 
 export default function Pricing() {
   return (
@@ -18,36 +17,4 @@ export default function Pricing() {
   )
 }
 
-function PricingPlan({ perks, mostPopular = false, planName, description, price }) {
-  return (
-    <div id='pricing' className="border-border dark:border-darkBorder dark:bg-brand-dark flex flex-col justify-between rounded-base border-2 bg-white p-5">
-      <div>
-        <div className="flex items-center justify-between">
-          <h3 className="text-2xl font-heading">{planName}</h3>
-          {mostPopular && (
-            <span className="border-border text-text dark:border-darkBorder rounded-base border-2 bg-brand-glow px-2 py-0.5 text-sm">
-              Most popular
-            </span>
-          )}
-        </div>
-        <p className="mb-3 mt-1">{description}</p>
-        <div>
-          <span className="text-3xl font-heading">${price}</span> <span>/month</span>
-        </div>
-        <ul className="mt-8 flex flex-col gap-2">
-          {perks.map((perk, i) => (
-            <li key={i} className="flex items-center gap-3">
-              <Check className="shrink-0" size={20} /> {perk}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Button
-        size={mostPopular ? 'lg' : 'default'}
-        className={cn('mt-12 w-full', mostPopular && 'bg-black text-white')}
-      >
-        Buy Plan
-      </Button>
-    </div>
-  )
-}
+
