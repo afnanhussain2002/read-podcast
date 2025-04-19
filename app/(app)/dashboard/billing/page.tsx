e96d4e9259d753562b5866fb324be35a837c6694
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-19 10:18:38 +0600

    fix issues of env file

--- app/(app)/dashboard/billing/page.tsx
+++ app/(app)/dashboard/billing/page.tsx
@@ -1,37 +1,38 @@
 "use client";
-import { subscribe } from '@/actions/stripe.action';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import React from 'react'
+import { subscribe } from "@/actions/stripe.action";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const BillingPage = () => {
-    const { data: session } = useSession();
-    const router = useRouter();
+  const { data: session } = useSession();
+  const router = useRouter();
 
-    const user = session?.user
+  const user = session?.user;
 
-    const handleSubscribe = async () => {
-        if (session?.user) {
-            throw new Error("User not found");
-        }
-
-        const url = await subscribe({
-            userId: user?.id as string,
-            email: user?.email as string,
-            priceId: process.env.STRIPE_SUBSCRIPTION_PRICE_ID as string
-        })
-
-        if (url) {
-           router.push(url)
-        }
+  const handleSubscribe = async () => {
+    if (!user) {
+      throw new Error("User not found");
     }
 
+    const url = await subscribe({
+      userId: user?.id as string,
+      email: user?.email as string,
+      priceId: process.env.NEXT_PUBLIC_STRIPE_SUBSCRIPTION_PRICE_ID!,
+    });
+
+    console.log(url);
+
+    if (url) {
+      router.push(url);
+    }
+  };
 
   return (
     <div>
-        <button onClick={handleSubscribe}>Subscribe</button>
+      <button onClick={handleSubscribe}>Subscribe</button>
     </div>
-  )
-}
+  );
+};
 
-export default BillingPage
+export default BillingPage;
