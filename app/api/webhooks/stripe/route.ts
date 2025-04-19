3b5eae4a36708cf75b6f955be89ae496611816ef
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-19 10:53:08 +0600

    import stripe

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,3 +2,5 @@ import {stripe} from "@/lib/stripe";
 import {NextResponse, NextRequest} from "next/server";
 import {headers} from "next/headers";
 import User from "@/models/User";
+import Stripe from "stripe";    
+
