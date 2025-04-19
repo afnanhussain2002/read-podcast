41e4d8d44401ba0df1dd28db43412ee245fe7932
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-19 07:07:00 +0600

    create customer and return success and cancel url

--- actions/stripe.action.ts
+++ actions/stripe.action.ts
@@ -27,6 +27,21 @@ export const subscribe = async ({ userId, email, priceId }: Props) => {
         })
         customerId = customer.id;
     }
+
+    const {url} = await stripe.checkout.sessions.create({
+        customer: customerId,
+        payment_method_types: ['card'],
+        line_items: [
+            {
+                price: priceId,
+                quantity: 1,
+            }
+        ],
+        mode: 'subscription',
+        success_url: 'http://localhost:3000/account',
+        cancel_url: 'http://localhost:3000/account'
+    })
+    return url;
    } catch (error) {
     
    }
