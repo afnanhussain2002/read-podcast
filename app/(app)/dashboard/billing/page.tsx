0332ba7c1393576adbd0c5f4e410f84e15e85b41
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-19 09:44:46 +0600

    get the subscribe func

--- app/(app)/dashboard/billing/page.tsx
+++ app/(app)/dashboard/billing/page.tsx
@@ -1,8 +1,21 @@
+import { subscribe } from '@/actions/stripe.action';
 import { useSession } from 'next-auth/react';
 import React from 'react'
 
 const BillingPage = () => {
     const { data: session } = useSession();
+
+    const handleSubscribe = async () => {
+        if (session?.user) {
+            throw new Error("User not found");
+        }
+
+        const url = await subscribe({
+            
+        })
+    }
+
+
   return (
     <div>BillingPage</div>
   )
