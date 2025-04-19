4da519a8fa3f9b4a1328fc8d05b176d3f047fc69
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-19 10:57:13 +0600

    add some variable on let func

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -7,4 +7,7 @@ import Stripe from "stripe";
 export async function POST(req: NextRequest) {
     const body = await req.text();
     const signature = (await headers()).get("Stripe-Signature");
+    let event: Stripe.Event;
+    let data: any;
+    let eventType: string;
 }
