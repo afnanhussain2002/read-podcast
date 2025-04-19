eaa4f4e682f0f5cae43a45fcb6dc5bd682677ca2
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-19 09:47:31 +0600

    send the props to stripe actions

--- app/(app)/dashboard/billing/page.tsx
+++ app/(app)/dashboard/billing/page.tsx
@@ -5,13 +5,17 @@ import React from 'react'
 const BillingPage = () => {
     const { data: session } = useSession();
 
+    const user = session?.user
+
     const handleSubscribe = async () => {
         if (session?.user) {
             throw new Error("User not found");
         }
 
         const url = await subscribe({
-            
+            userId: user?.id as string,
+            email: user?.email as string,
+            priceId: process.env.STRIPE_SUBSCRIPTION_PRICE_ID as string
         })
     }
 
