47e391a8e5b429de980c97b11af773e40ced1775
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-21 09:39:05 +0600

    connect the database

--- app/api/delete-all/route.ts
+++ app/api/delete-all/route.ts
@@ -1,11 +1,20 @@
 import { authOptions } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(req: NextRequest) { 
    const session = await getServerSession(authOptions);
+
+   const userId = session?.user?.id;
    
-      if (!session || !session.user) {
+      if (!userId) {
           return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
+
+      try {
+        await connectToDatabase();
+      } catch (error) {
+        
+      }  
 }
