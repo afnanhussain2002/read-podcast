f715d496aaeab26f1f722c571cb80cb551e44980
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-21 08:28:01 +0600

    make the loading components

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Star10 from './stars/s10'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="flex items-center justify-center h-screen">
+                <Star10 size={100} strokeWidth={4} className="animate-spin text-blue-500" />
+              </div>
   )
 }
 
