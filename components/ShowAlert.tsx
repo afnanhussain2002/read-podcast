2ca73e3308f54d831d0ea8bb365d7144b351109f
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-21 10:14:47 +0600

    import alert files

---
+++ components/ShowAlert.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from '@/components/ui/alert-dialog'
+  import { Button } from '@/components/ui/button'
+  
+
+const ShowAlert = () => {
+  return (
+    <div>ShowAlert</div>
+  )
+}
+
+export default ShowAlert
