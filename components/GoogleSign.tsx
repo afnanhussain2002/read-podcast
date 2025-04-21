33a18b9df2af214f8705d22b9f84c3e86ed27deb
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-21 10:39:55 +0600

    add loading for google sign

--- components/GoogleSign.tsx
+++ components/GoogleSign.tsx
@@ -1,17 +1,38 @@
 "use client";
+
+import { useState } from "react";
 import { signIn } from "next-auth/react";
-import { LogIn } from 'lucide-react';
+import { LogIn, Loader2 } from "lucide-react";
+
 
 export default function GoogleSignIn() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGoogleSignIn = async () => {
+    setIsLoading(true);
+    await signIn("google", { callbackUrl: "/" });
+    // No need to set isLoading back to false because it redirects
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="bg-white rounded-lg shadow-lg w-full">
         <button
-            onClick={() => signIn('google', { callbackUrl: '/' })}
+          onClick={handleGoogleSignIn}
           className="w-full flex items-center justify-center gap-2 bg-white text-gray-700 border border-gray-300 py-2 px-4 rounded-lg shadow-sm hover:bg-gray-100 transition-all"
+          disabled={isLoading}
         >
-          <LogIn className="text-2xl" />
-          <span className="font-medium">Sign in with Google</span>
+          {isLoading ? (
+            <>
+              <Loader2 className="animate-spin h-5 w-5" />
+              <span className="font-medium">Signing in...</span>
+            </>
+          ) : (
+            <>
+              <LogIn className="text-2xl" />
+              <span className="font-medium">Sign in with Google</span>
+            </>
+          )}
         </button>
       </div>
     </div>
