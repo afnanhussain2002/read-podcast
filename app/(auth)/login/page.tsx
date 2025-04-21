194d49ff499c206a6d4eb387fd3a8fd10f6d630f
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-21 10:38:18 +0600

    add loading on login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,7 +5,14 @@ import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useNotification } from "@/components/Notification";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -15,10 +22,14 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
   const { showNotification } = useNotification();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
+
     const result = await signIn("credentials", {
       email,
       password,
@@ -31,44 +42,48 @@ export default function Login() {
       showNotification("Login successful!", "success");
       router.push("/");
     }
+
+    setIsLoading(false);
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen p-4 dark:bg-brand-darkBg">
-    <Card className="w-full max-w-sm mx-auto">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>Enter your credentials to access your account</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              type="password"
-              placeholder="••••••••"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </form>
-        <div className="relative my-4">
+      <Card className="w-full max-w-sm mx-auto">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your credentials to access your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="••••••••"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <Button type="submit" className="w-full" disabled={isLoading}>
+              {isLoading ? "Logging in..." : "Login"}
+            </Button>
+          </form>
+          <div className="relative my-4">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t" />
             </div>
@@ -76,18 +91,20 @@ export default function Login() {
               <span className="bg-white px-2 text-muted-foreground">or</span>
             </div>
           </div>
-        <GoogleSignIn />
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="/register" className="underline underline-offset-4 font-bold hover:text-primary">
-            Register
-          </Link>
-        </div>
-      </CardFooter>
-    </Card>
-
-      </div>
+          <GoogleSignIn />
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link
+              href="/register"
+              className="underline underline-offset-4 font-bold hover:text-primary"
+            >
+              Register
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
