3426004b0c21c0072bd5424056666b12b696be20
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-23 06:04:21 +0600

    add a validation for type email

--- app/api/forget-password/route.ts
+++ app/api/forget-password/route.ts
@@ -1,5 +1,11 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export default async function POST(req: NextRequest) {
     const {email} = await req.json();
+
+    if (!email) {
+        return NextResponse.json({ error: "Email is required" }, { status: 400 });
+    }
+
+    
 }
