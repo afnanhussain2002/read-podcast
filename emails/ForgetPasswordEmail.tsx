29b8d5e710c4e414297eed47273b434cdd46c9ca
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-23 16:07:35 +0600

    write a simple template

--- emails/ForgetPasswordEmail.tsx
+++ emails/ForgetPasswordEmail.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const ForgetPasswordEmail = () => {
-  return (
-    <div>ForgetPasswordEmail</div>
-  )
+interface FPEmailProps {
+  firstName: string;
 }
 
+const ForgetPasswordEmail: React.FC<Readonly<FPEmailProps>> = ({firstName})  => {
+  return (
+    <div>
+    <h1>Welcome, {firstName}!</h1>
+  </div>
+  )
+}
+        
 export default ForgetPasswordEmail
