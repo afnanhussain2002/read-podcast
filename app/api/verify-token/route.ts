8113160b7aca45d3650404699b713301158b8da0
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-24 11:06:24 +0600

    get hashed token and get the user

--- app/api/verify-token/route.ts
+++ app/api/verify-token/route.ts
@@ -1,7 +1,15 @@
-import { connectToDatabase } from "@/lib/db"
-import crypto from "crypto"
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import crypto from "crypto";
 
 export async function POST(req: Request) {
-    const {email} = await req.json()
-    await connectToDatabase()
+  const { token } = await req.json();
+  await connectToDatabase();
+
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+
+  const user = await User.findOne({
+    resetToken: hashedToken,
+    resetTokenExpiry: { $gt: Date.now() },
+  });
 }
