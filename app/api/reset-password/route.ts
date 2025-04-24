28433285d2e5e98f05840579890a370ae316ae15
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-24 11:26:07 +0600

    write a try catch block

--- app/api/reset-password/route.ts
+++ app/api/reset-password/route.ts
@@ -3,6 +3,13 @@ import { connectToDatabase } from "@/lib/db";
 export async function POST(req: Request) {
     const { password, email } = await req.json();
 
-    await connectToDatabase();
+    try {
+        await connectToDatabase();
+    } catch (error) {
+        
+    }
+
+    
+
   
 }
