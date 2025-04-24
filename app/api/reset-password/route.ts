bde4d14e75779740224370c085c1ca74bdb48b4d
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-24 11:31:28 +0600

    update needed data of existing user

--- app/api/reset-password/route.ts
+++ app/api/reset-password/route.ts
@@ -1,6 +1,7 @@
 import { connectToDatabase } from "@/lib/db";
 import User from "@/models/User";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
     const { password, email } = await req.json();
@@ -13,6 +14,17 @@ export async function POST(req: Request) {
         if (!existingUser) {
             return NextResponse.json({ error: "User not found" }, { status: 404 });
         }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        existingUser.password = hashedPassword;
+        existingUser.resetToken = undefined;
+        existingUser.resetTokenExpiry = undefined;
+
+        await existingUser.save();
+
+        
+
 
     } catch (error) {
         
