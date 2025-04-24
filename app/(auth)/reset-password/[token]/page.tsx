22ea3a1bedb33132fcddccbeb05d512c9e545604
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-24 11:16:36 +0600

    set user to that user

--- app/(auth)/reset-password/[token]/page.tsx
+++ app/(auth)/reset-password/[token]/page.tsx
@@ -30,6 +30,7 @@ export default function ResetPasswordPage({ params }: any) {
       if (res.status === 200) {
         setVerified(true);
         const userData = await res.json();
+        setUser(userData.user);
       }
     };
     
