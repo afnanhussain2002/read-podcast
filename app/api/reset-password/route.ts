9a4d215e8ff4c07a9f24b20f072d4e21bd19ea6b
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-24 11:25:45 +0600

    connect with database

--- app/api/reset-password/route.ts
+++ app/api/reset-password/route.ts
@@ -1,6 +1,8 @@
+import { connectToDatabase } from "@/lib/db";
+
 export async function POST(req: Request) {
     const { password, email } = await req.json();
 
-    
+    await connectToDatabase();
   
 }
