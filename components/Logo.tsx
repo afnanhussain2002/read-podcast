9c5ce6f362124046ffec852703bcf95805b78810
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-25 06:35:35 +0600

    import next image and logo

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from '../public/logo.png'
+
+const Logo = () => {
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
