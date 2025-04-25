92662c3d94f21a2840fedde5b858d5f378910425
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-25 06:37:11 +0600

    write the code for logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import logo from '../public/logo.png'
+import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <Link href={"/"}>
+    <Image src={logo} alt="logo" width={100} height={100} className='cursor-pointer rounded-md'/>
+    
+    </Link>
   )
 }
 
