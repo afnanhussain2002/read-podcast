fb306302d969c8bff3778b9b7f24de46144d23eb
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-26 15:13:44 +0600

    connect with database

--- app/api/summary/[audioUrl]/route.ts
+++ app/api/summary/[audioUrl]/route.ts
@@ -1,8 +1,10 @@
+import { connectToDatabase } from "@/lib/db";
+
 export async function POST({ params }: { params: { audioUrl: string } }) {
     const { audioUrl } = await params;
 
     try {
-        
+        await connectToDatabase();
     } catch (error) {
         
     }
