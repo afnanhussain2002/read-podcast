24430c6f47ab92b381e44bb17860654e01f40cd0
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-26 15:13:29 +0600

    write a try catch block

--- app/api/summary/[audioUrl]/route.ts
+++ app/api/summary/[audioUrl]/route.ts
@@ -1,6 +1,12 @@
 export async function POST({ params }: { params: { audioUrl: string } }) {
     const { audioUrl } = await params;
 
+    try {
+        
+    } catch (error) {
+        
+    }
+
 
     
 }
