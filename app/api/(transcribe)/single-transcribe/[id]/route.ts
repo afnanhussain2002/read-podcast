ea7827ebcca620e8a5dd8704b8da590171c44192
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-27 21:36:38 +0600

    add an alternative method

--- app/api/(transcribe)/single-transcribe/[id]/route.ts
+++ app/api/(transcribe)/single-transcribe/[id]/route.ts
@@ -2,12 +2,17 @@ import { connectToDatabase } from "@/lib/db";
 import Transcript from "@/models/Transcript";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, {params}: {params: {id: string}}) {
+interface RouteParams {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(request: NextRequest, { params }: RouteParams) {
   const id = params.id;
 
   try {
     await connectToDatabase();
-
     const transcript = await Transcript.findById(id);
 
     if (!transcript) {
@@ -16,6 +21,9 @@ export async function GET(request: NextRequest, {params}: {params: {id: string}}
 
     return NextResponse.json({ transcript }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: error as string }, { status: 500 });
+    return NextResponse.json(
+      { error: error as string}, 
+      { status: 500 }
+    );
   }
 }
