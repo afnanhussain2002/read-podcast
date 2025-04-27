323d873337483ccc233c7aad6b728f7662ec1411
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-27 11:21:11 +0600

    add shadow

--- components/singleMenu.tsx
+++ components/singleMenu.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SingleMenu = () => {
   return (
     <div className='mt-20 flex fixed'>
-        <ul className='bg-brand-glow p-2 rounded-md flex gap-4 flex-col font-bold text-sm'>
+        <ul className='bg-brand-glow p-2 rounded-md flex gap-4 flex-col font-bold text-sm shadow-light dark:shadow-dark'>
             <a href='#fullTranscript'>Full Transcript</a>
             <a href='#chapters'>Chapters</a>
             <a href='#entities'>Entities</a>
