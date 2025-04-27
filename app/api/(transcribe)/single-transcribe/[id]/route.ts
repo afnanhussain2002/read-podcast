cb94fa58ef44b5543dfd02f00529473ad146c3f0
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-27 20:59:08 +0600

    remove next response

--- app/api/(transcribe)/single-transcribe/[id]/route.ts
+++ app/api/(transcribe)/single-transcribe/[id]/route.ts
@@ -1,12 +1,9 @@
 import { connectToDatabase } from "@/lib/db";
 import Transcript from "@/models/Transcript";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 // ✅ Named export for GET request
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function GET({ params }: { params: { id: string } }) {
   const { id } = params;
 
   try {
@@ -15,12 +12,18 @@ export async function GET(
     const transcript = await Transcript.findById(id);
 
     if (!transcript) {
-      return NextResponse.json({ error: "Transcript not found" }, { status: 404 });
+      return NextResponse.json(
+        { error: "Transcript not found" },
+        { status: 404 }
+      );
     }
 
     return NextResponse.json(transcript, { status: 200 }); // no need to wrap in { transcript }
   } catch (error) {
     console.error("Error fetching transcript:", error);
-    return NextResponse.json({ error: "Failed to get the transcript" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to get the transcript" },
+      { status: 500 }
+    );
   }
 }
