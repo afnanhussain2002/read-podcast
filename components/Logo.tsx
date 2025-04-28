16b9881778c021aaf95c9024822c6f37636611ba
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-28 18:14:02 +0600

    logo make responsive

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const Logo = () => {
   return (
     <Link href={"/"}>
-    <Image src={logo} alt="logo" width={200} height={200} className='cursor-pointer rounded-md shadow-light dark:shadow-dark'/>
+    <Image src={logo} alt="logo" width={100} height={100} className='w-28 h-auto cursor-pointer rounded-md shadow-light dark:shadow-dark lg:w-52'/>
     
     </Link>
   )
