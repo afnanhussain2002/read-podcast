64d921ba7faf7f9f9c6f27230c8c66df8c65725a
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-04-29 22:16:26 +0600

    change a name of html tag

--- app/(app)/transcriber/page.tsx
+++ app/(app)/transcriber/page.tsx
@@ -1,11 +1,10 @@
 import TranscribeInput from "@/components/TranscribeInput";
 
-
 export default function Transcriber() {
 
   return (
     <>
-     <header className="dark:bg-brand-dark inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+     <section className="dark:bg-brand-dark inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
     <div className="mx-auto w-container max-w-full px-5 py-[110px] text-center lg:py-[150px]">
       <h1 className="text-3xl font-heading md:text-4xl lg:text-5xl">
       ✨ Transcribe Anything, Instantly
@@ -18,7 +17,7 @@ export default function Transcriber() {
       <TranscribeInput/>
       
     </div>
-  </header>  
+  </section>  
     
     </>
   );
