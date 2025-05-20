3aa3d20da43caa0242dfa4c1523aeb8e63484fae
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-05-20 22:53:38 +0600

    remove use sequre

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,7 +6,6 @@ import User from "@/models/User";
 import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
-  useSecureCookies: process.env.NODE_ENV === "production",
   providers: [
     // Credentials login
     CredentialsProvider({
