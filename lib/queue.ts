abcf23b8fb9c2d08222ea6529debd09a72c3dc29
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-05-23 19:16:44 +0600

    remove un used file

--- lib/queue.ts
+++
@@ -1,6 +0,0 @@
-import { Queue } from 'bullmq';
-import { redisConnection } from './redis';
-
-export const transcriptionQueue = new Queue('transcribeQueue', {
-  connection: redisConnection,
-});
--- lib/redis.ts
+++
@@ -1,6 +0,0 @@
-import { RedisOptions } from 'bullmq';
-
-export const redisConnection: RedisOptions = {
-  host: process.env.REDIS_HOST || '127.0.0.1',
-  port: parseInt(process.env.REDIS_PORT || '6379'),
-};
