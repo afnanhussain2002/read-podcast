b1d047590ab413a5b628f7fa9e8660a6f27aad9c
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-05-24 09:38:54 +0600

    change some text

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -6,7 +6,7 @@ export default function Pricing() {
   return (
     <section id="pricing" className="py-20 font-base lg:py-[100px] bg-brand-light dark:bg-brand-dark border-t-2 border-border dark:border-darkBorder">
       <h2 className="mb-14 px-5 text-center text-2xl font-heading md:text-3xl lg:mb-20 lg:text-4xl">
-        🚀 Choose the Perfect Plan for You
+        🚀 No Subscription, Pay as you Go
       </h2>
       <div className="mx-auto grid w-container max-w-full grid-cols-1 gap-5 px-5 sm:grid-cols-2 lg:grid-cols-3">
         {pricingPlans.map((plan, index) => (
