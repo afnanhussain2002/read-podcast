7e3cdcd25e96dc57f5ef2566b5006baee8953dc1
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-05-24 06:02:37 +0600

    add webhook to publik

--- middleware.ts
+++ middleware.ts
@@ -26,7 +26,7 @@ export default withAuth(
           pathname.startsWith("/api/forget-password") ||
           pathname.startsWith("/api/reset-password") ||
           pathname.startsWith("/api/verify-token") ||
-          pathname.startsWith("/api/webhooks/stripe") ||
+          pathname.startsWith("/api/stripe/webhook") ||
           pathname.startsWith("/api/assemblyAi/webhook")
         ) {
           return true;
